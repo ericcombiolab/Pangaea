19cc986a01732be23a5a9b5a54799fd009031fe7
jxmelody <jmelody.xiao@gmail.com>
2024-06-20 21:36:59 +0800

    update installation

--- bin/calculate_diversity.sh
+++ bin/calculate_diversity.sh
@@ -4,9 +4,20 @@
 # wget http://cmprod1.cibio.unitn.it/biobakery4/metaphlan_databases/mpa_vOct22_CHOCOPhlAnSGB_202212.tar
 threads=`nproc`
 input_reads=$1
+if [ ! -f $input_reads ];then
+    echo "Error: $input_reads not found"
+    exit 1
+fi
+
 # this file's path
 script_path=$(dirname "$0")
 mkdir -p ./metaphlan_tmp
-metaphlan $input_reads --input_type fastq --bowtie2db $DATA/metaphlan/ --nproc $threads --bowtie2out metaphlan_tmp/metagenome_from_reads.bowtie2.bz2 -o metaphlan_tmp/profiled.txt
+metaphlan_db=$2
+if [ ! -d $metaphlan_db ];then
+    echo "Not provide the metaphlan database path, use the default one"
+    metaphlan $input_reads --input_type fastq --nproc $threads --bowtie2out metaphlan_tmp/metagenome_from_reads.bowtie2.bz2 -o metaphlan_tmp/profiled.txt
+else
+    metaphlan $input_reads --input_type fastq --bowtie2db $metaphlan_db --nproc $threads --bowtie2out metaphlan_tmp/metagenome_from_reads.bowtie2.bz2 -o metaphlan_tmp/profiled.txt
+fi
 python $script_path/metaphlan_tables.py  metaphlan_tmp/profiled.txt metaphlan_tmp/profiled.txt > profiles_table.tsv
 Rscript $script_path/calculate_diversity.R -d alpha -m shannon -f  profiles_table.tsv -o metaphlan_tmp/diversity_analysis
