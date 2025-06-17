363786b996d43fea78d3b396daf36cfc2b24088e
Zhenmiao Zhang <zhz142@ucsd.edu>
2025-06-16 19:33:41 -0700

    fix ordering in build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-# install pytorch according to https://pytorch.org/get-started/locally/
-# for example, if cpu only
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
-
 # install conda env pangaea
 conda install conda-forge::mamba
 mamba env create -f environment.yaml && conda activate pangaea
+
+# install pytorch according to https://pytorch.org/get-started/locally/
+# for example, if cpu only
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 
 # install rph_kmeans
 cd third_parties/rph_kmeans
